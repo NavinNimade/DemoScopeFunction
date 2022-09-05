1dbdbd044fbc9cf44ff7ef8a7386c7c951c13141
BTPL-L-446 <btpl-l-446@Koos-MacBook-Pro-3.local>
2022-09-05 17:29:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mvvpdemoapplication/DemoScope.kt
@@ -0,0 +1,8 @@
+package com.example.mvvpdemoapplication
+
+data class Employee(var name:String ="navin", var age : Int = 35)
+
+fun main()
+{
+    var employee : Employee? =null
+}
---
+++ app/src/main/java/com/example/mvvpdemoapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mvvpdemoapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
